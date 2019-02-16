bd8778b4abf16ab1bdac2edbff3b6105402593a5
Swapnil Patil <Swapnil>
2019-02-16 16:50:22 +0800

    [FE] added ga

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import Loadable from 'react-loadable';
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import './assets/css/global.css';
 import './assets/css/animations.css';
 import App from './containers/App';
 import configureStore from './store';
+
+// ga
+// ReactGA.initialize('UA-50888218-1', { standardImplementation: true });
+// ReactGA.pageview(window.location.pathname + window.location.search);
 
 const store = configureStore(window.__REDUX_STATE__);
 
